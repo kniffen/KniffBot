fe48b1e54c1b582510f4dfcb33514a3bc749728c
Kniffen <knifftech@gmail.com>
2018-11-03 05:51:48 +0100

    Removed argument

--- source/lib/commands/time.js
+++ source/lib/commands/time.js
@@ -1,23 +1,10 @@
 async function time(
   message,
-  date = Date,
   state = require('../state.js'),
-  moment = require('moment-timezone'),
-  fetch = require('node-fetch')
+  moment = require('moment-timezone')
 ) {
 
-  if (!message.args.length) {
-    message.output = moment.tz(state.timezone).format(state.timeformat)
-  
-  } else {
-    const geopath = 'http://maps.googleapis.com/maps/api/geocode/json?address='+encodeURI(message.args.join(' ').replace(/&/g, ''))
-    const { lat, lng } = (await fetch(geopath).then(res => res.json())).results[0].geometry.location
-    const tzpath = `https://maps.googleapis.com/maps/api/timezone/json?location=${lat},${lng}&timestamp=0&sensor=false`
-    const timezone = (await fetch(tzpath).then(res => res.json())).timeZoneId
-    
-    message.output = moment.tz(date.now(), timezone).format(state.timeformat) + ' ' + timezone
-  }
-
+  message.output = moment.tz(state.timezone).format(state.timeformat)
   message.isCode = true
 
   return message
--- source/spec/commands/time.spec.js
+++ source/spec/commands/time.spec.js
@@ -7,10 +7,6 @@ chai.use(spies)
 
 describe('commands/time()', () => {
 
-  const date = {
-    now: () => 100
-  }
-
   const state = {
     timezone: 'foo',
     timeformat: 'bar'
@@ -19,24 +15,9 @@ describe('commands/time()', () => {
   const format = chai.spy(() => 'foobar')
   const tz = chai.spy(() => ({format}))
   const moment = {tz}
-  
-  const json = chai.spy(async () => ({
-    results: [{
-      geometry: {
-        location: {
-          lat: 1,
-          lng: 2
-        }
-      }
-    }],
-    timeZoneId: 'qux'
-  }))
-
-  const fetch = chai.spy(async () => ({json}))
-
 
   it('should return the current time', async () => {
-    const message = await time({args: []}, date, state, moment, fetch)
+    const message = await time({args: []}, state, moment)
 
     expect(tz).to.have.been.called.with('foo')
     expect(format).to.have.been.called.with('bar')
@@ -47,19 +28,6 @@ describe('commands/time()', () => {
     })
   })
 
-  it('should return the time for a given location', async () => {
-    const message = await time({args: ['london', 'uk']}, date, state, moment, fetch)
-
-    expect(fetch).to.have.been.called.with('http://maps.googleapis.com/maps/api/geocode/json?address=london%20uk')
-    expect(json).to.have.been.called()
-    expect(fetch).to.have.been.called.with('https://maps.googleapis.com/maps/api/timezone/json?location=1,2&timestamp=0&sensor=false')
-    expect(tz).to.have.been.called.with(100, 'qux')
-    expect(format).to.have.been.called.with('bar')
-    expect(message).to.deep.equal({
-      args: ['london', 'uk'],
-      isCode: true,
-      output: 'foobar qux'
-    })
-  })
+  it('should return the time for a given location')
 
 })
