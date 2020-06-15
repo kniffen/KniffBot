a6eee1e08cfe82960a1dff3c3e4756724fd0dd37
Kniffen <knifftech@gmail.com>
2020-06-15 11:04:54 +0200

    Added restrictions to commands

--- source/bot/commands/cat.js
+++ source/bot/commands/cat.js
@@ -5,10 +5,11 @@
 
 import fetch from "node-fetch"
 
-export const id       = "cat"
-export const category = "fun"
-export const services = ["discord", "twitchIRC"]
-export const args     = [[]]
+export const id           = "cat"
+export const category     = "fun"
+export const services     = ["discord", "twitchIRC"]
+export const args         = [[]]
+export const isRestricted = false
 
 export default async function catCmd(message) {
 
