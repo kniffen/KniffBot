80ce2b12f0f2fb7ea3c15ef14ede29b478a0147b
Kniffen <knifftech@gmail.com>
2020-06-17 11:27:14 +0200

    Update log.js

--- source/bot/utils/log.js
+++ source/bot/utils/log.js
@@ -8,7 +8,7 @@
 
 import moment from "moment-timezone"
 
-export default function log({ label, message, isError }) {
+export default function log({ label, message }) {
 
   console.log(`[${label || "SYSTEM"}] ${moment.utc().format("YYYY-MM-DD HH:mm:ss z")}: ${message}`)
 
